4df5210b83aa4089adef36fdf45dc08c6090c841
Per Samuelsson <per@starcounter.com>
2015-12-19 12:06:44 +0100

    Implemented possibility to enter the application name / path and to press "Ship it" button.

--- src/AppShip/AppShip.json.cs
+++ src/AppShip/AppShip.json.cs
@@ -1,6 +1,29 @@
 using Starcounter;
+using System;
+using System.Linq;
 
 namespace AppShip {
+
     partial class AppShip : Partial {
+
+        void Handle(Input.ShipIt shipit) {
+            var appSpec = Application;
+
+            if (string.IsNullOrEmpty(appSpec)) {
+                ApplicationFiles.Clear();
+                return;
+            }
+
+            var app = Db.Applications.FirstOrDefault(a => a.Name.Equals(appSpec, StringComparison.InvariantCultureIgnoreCase));
+            if (app == null) {
+                ApplicationFiles.Clear();
+                return;
+            }
+
+            OutDirectory = string.Format("AppShip-{0}-{1}", Db.Environment.DatabaseNameLower, app.Name);
+
+            ApplicationFiles.Clear();
+            ApplicationFiles.Add().Path = app.FilePath;
+        }
     }
 }
