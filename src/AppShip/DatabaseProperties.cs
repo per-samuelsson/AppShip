cbac16ec556ac9f691dd7dfed36705a07646aed4
Per Samuelsson <per@starcounter.com>
2015-12-21 12:08:39 +0100

    Introduced simplistic DatabaseProperties class, and a IDatabaseProperties interface, allowing consumption of database settings from within an application.

---
+++ src/AppShip/DatabaseProperties.cs
@@ -0,0 +1,49 @@
+﻿
+using Starcounter;
+using Starcounter.Internal;
+
+namespace AppShip {
+
+    public interface IDatabaseProperties {
+        string ImageDirectory { get; }
+        string TransactionLogDirectory { get; }
+        string TempDirectory { get; }
+    }
+
+    public class DatabaseProperties : IDatabaseProperties {
+        string imageDir;
+        string tlogDir;
+        string tempDir;
+
+        string IDatabaseProperties.ImageDirectory {
+            get {
+                return imageDir;
+            }
+        }
+
+        string IDatabaseProperties.TempDirectory {
+            get {
+                return tempDir;
+            }
+        }
+
+        string IDatabaseProperties.TransactionLogDirectory {
+            get {
+                return tlogDir;
+            }
+        }
+
+        public static IDatabaseProperties CreateFromServerRequest() {
+            var serverPort = StarcounterEnvironment.Default.SystemHttpPort;
+            var x = Http.GET(string.Format("http://localhost:{0}/api/admin/databases/{1}/settings", serverPort, Db.Environment.DatabaseNameLower));
+            var j = new Json(x.ToString());
+
+            var props = new DatabaseProperties();
+            props.imageDir = j["ImageDirectory"] as string;
+            props.tlogDir = j["TransactionLogDirectory"] as string;
+            props.tempDir = j["TempDirectory"] as string;
+
+            return props;
+        }
+    }
+}
